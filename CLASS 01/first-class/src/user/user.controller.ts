87feca90dc8e9c64923bf6e72535abd3a855a8cb
Qadirdad-Kazi <qadirdadkazi@gmail.com>
2025-04-19 12:56:32 +0500

    Update user.controller.ts

--- CLASS 01/first-class/src/user/user.controller.ts
+++ CLASS 01/first-class/src/user/user.controller.ts
@@ -1,21 +1,23 @@
-import { Controller, Get, Post, Param, Body, UseGuards, UseInterceptors, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, UseGuards, ParseIntPipe, UseInterceptors, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDto } from './dto/userDto';
 import { LoggingInterceptor } from 'src/Interceptor/loginInterceptor';
-import { AuthGuard } from 'src/Guard/auth.guard'; // or 'src/Guard/auth/auth.guard' if not moved
+import { PlainLiteralObject } from '@nestjs/common';
+import { UserResponseDto } from './dto/userDtoResponse';
+import { AuthGuard } from 'src/Guard/auth.guard';
+
+
 
 @Controller('user')
 @UseInterceptors(LoggingInterceptor)
+@UsePipes(ParseIntPipe)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get(':id')
   @UseGuards(AuthGuard)
-  getUser(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.getUser(id);
+  getUser(@Param('id') id: ParseIntPipe) {
+    return this.userService.getUser(Number(id));
   }
 
-  @Post()
-  createUser(@Body() newUser: { name: string; email: string }) {
-    return this.userService.createUser(newUser);
-  }
 }
